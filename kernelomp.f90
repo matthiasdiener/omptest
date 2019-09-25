66c32d6e27d225e76d636a1e268381108b08c389
Matthias Diener <mdiener@illinois.edu>
2019-09-25 09:33:52 -0700

    add ICE test function

--- kernelomp.f90
+++ kernelomp.f90
@@ -14,3 +14,38 @@ SUBROUTINE ZAXPY(START,END,LEN,X,Y,Z)
   END DO
   
 END SUBROUTINE ZAXPY
+
+
+!ICE test - base function
+SUBROUTINE ZAXPY_BASE(START,END,LEN,X,Y,Z,A)
+
+  INTEGER(KIND=8), INTENT(IN)  :: START,END,LEN
+  REAL(KIND=8),    INTENT(IN)  :: X(LEN),A
+  REAL(KIND=8),    INTENT(IN)  :: Y(LEN)
+  REAL(KIND=8),    INTENT(OUT) :: Z(LEN)
+
+  INTEGER(KIND=8) :: I
+
+  DO I = START, END
+    Z(I) = A * X(I) + Y(I)
+  END DO
+
+END SUBROUTINE ZAXPY_BASE
+
+
+!ICE test - offloaded function
+SUBROUTINE ZAXPY_MOD(START,END,LEN,X,Y,Z,A)
+
+  INTEGER(KIND=8), INTENT(IN)  :: START,END,LEN
+  REAL(KIND=8),    INTENT(IN)  :: X(LEN),A
+  REAL(KIND=8),    INTENT(IN)  :: Y(LEN)
+  REAL(KIND=8),    INTENT(OUT) :: Z(LEN)
+
+  INTEGER(KIND=8) :: I
+
+  !$omp target teams distribute parallel do simd is_device_ptr(X,Y,Z) num_teams(80) thread_limit(2000)
+  DO I = START, END
+    Z(I) = A * X(I) + Y(I)
+  END DO
+
+END SUBROUTINE ZAXPY_MOD
